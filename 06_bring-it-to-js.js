724721f522accec4b1cfcd893ced1f1594626cd3
AkkarawinS <akkarawinRX@gmail.com>
2026-03-10 15:53:08 +0700

    add some details in bring-it-to-js.js file

---
+++ 06_bring-it-to-js.js
@@ -0,0 +1,24 @@
+let customer = ["customer_name","customer_email","password","address","phone_number"];
+
+let order1 = {
+    Data:"10/03/2026",
+    Time:"3:46 PM",
+    OrderNumber:"0001",
+    Total_Price:200,
+    orderitemid:"11500",
+};
+
+let orderitem01 ={
+    orderitemid:"11500",
+    productid:"002",
+    OrderNumber:"0001",
+    Quantity:2,
+    Price:50,
+}
+let product_matcha = {
+    ProductName:"Matcha",
+    productid:"002",
+    Price:50,
+    stock:100,
+    describtion:"iced matcha greentea from a local supplier",
+}
